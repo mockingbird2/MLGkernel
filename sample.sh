0f06e1341a06f7a8ff8c436fa329ffa8140476bd
Fabian Paul <fabian.paul@student.hpi.de>
2018-07-05 14:55:54 +0200

    Make thread count configurable

--- sample.sh
+++ sample.sh
@@ -10,7 +10,7 @@
 dset=$DSET
 data=$DATA
 save=$OUTPUT/$DSET\_output.txt
-
+workers=$WORKERS
 cd MLGkernel
-./runMLG -d $data -s $save -r 1 -l 2 -e 0.1 -g 0.01 -t 32 -m 1
+./runMLG -d $data -s $save -r 1 -l 2 -e 0.1 -g 0.01 -t $workers -m 1
 cd ../
